1a54fc2fa820bd215c5ff2254f77b51a1a8206a1
Simon O'Hara <simon.ohara@sage.com>
2012-02-03 23:07:00 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Couture::Application.config.secret_token = '86c6cc2599363f21faf465f9fea316ddbcb8a9092a7c1e2950773ef6bcb4484c06c959f8323ece97697850226dbceee142915024e288aedc72b5c4d39cd1b384'
